d27b3a3f01ed88ae660931a7d8fe8b3e1cb90dec
mdtanker <matt.d.tankersley@gmail.com>
2022-10-29 16:34:34 +1300

    first chuck of files added

--- git_push.sh
+++ git_push.sh
@@ -2,8 +2,8 @@
 if tasklist | grep --quiet zotero; then # returns 0 if zotero process was found, 1 elsewise
 read -n1 -r -p "Commit failed, please exit Zotero first!" key
 else
-echo "testing"
-#git add .
-#git commit -m "`date +\"%Y%m%d-%H%M%S\"`"
-#git push origin master
+echo "run git add, commit, and push"
+git add .
+git commit -m "`date +\"%Y%m%d-%H%M%S\"`"
+git push origin main
 fi 
